8eebaa249b9b0759052039cf36ceba0d5e66eead
John Dziurlaj <jdziurlaj@gmail.com>
2018-06-08 12:19:58 -0400

    better example for xor

--- test/validate.spec.ts
+++ test/validate.spec.ts
@@ -31,37 +31,35 @@ describe("conditional validation", async function () {
 
 });
 describe("exclusive or", async function () {
-    const exclusiveConstraint = `inv catsMeowDogsBark: (self.kind = "cat" and self.sound = "meow")
-            xor (self.kind = "dog" and self.sound = "bark")`;
+    const exclusiveConstraint = `inv serviceOrProduct: self.service->oclIsUndefined() xor self.product->oclIsUndefined()`;            
     it("should validate successfully on exclusive constraint", async function () {
 
         @ContextFor(exclusiveConstraint)
-        class Pet {
-            kind?: string;
-            sound?: string;
+        class SoldVia {
+            product?: object;
+            service?: object;
         }
 
-        const pet = new Pet();
-        pet.kind = "cat"
-        pet.sound = "meow";
+        const soldVia = new SoldVia();
+        soldVia.product = {};
 
-        const errors = await validator.validate(pet);
+        const errors = await validator.validate(soldVia);
         expect(errors.length).to.equal(0);
     });
     it("should validate successfully on exclusive constraint", async function () {
 
         @ContextFor(exclusiveConstraint)
-        class Pet {
-            kind?: string;
-            sound?: string;
+        class SoldVia {
+            product?: object;
+            service?: object;
         }
 
-        const pet = new Pet();
-        pet.kind = "dog"
-        pet.sound = "meow";
-
-        const errors = await validator.validate(pet);
-        expect(errors).to.include("catsMeowDogsBark");
+        const soldVia = new SoldVia();
+        soldVia.product = {};
+        soldVia.service = {};
+        
+        const errors = await validator.validate(soldVia);
+        expect(errors).to.include("serviceOrProduct");
     });
 });
 describe("exclusive or", async function () {
